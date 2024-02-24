7360d24577a498f1f69e37c1e5617e8506d68940
akhil2099 <akhilvimal7@gmail.com>
2024-02-24 20:42:19 +0530

    SonarQube testing 2

--- sum.py
+++ sum.py
@@ -3,7 +3,7 @@ def add_numbers(a, b):
 
 # Example usage:
 num1 = 5
-num2 = 3
+num = 3
 result = add_numbers(num1, num2)
-print("The sum of", num, "and", num2, "is:", result)
+print("The sum of", num1, "and", num2, "is:", result)
 
