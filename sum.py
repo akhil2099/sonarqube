b8b03bc7c09b351244f7f6b164b7a52f3052c1c1
akhil2099 <akhilvimal7@gmail.com>
2024-02-24 20:55:15 +0530

    SonarQube testing 3

--- sum.py
+++ sum.py
@@ -1,9 +1,12 @@
 d ef add_numbers(a, b):
     re turn a + b
 
-# Example usage:
-num1 = 5
-num = 3
-result = add_numbers(num1, num2)
-print("The sum of", num1, "and", num2, "is:", result)
+def main():
+    num1 = 5
+    num = 3
+    result = add_numbers(num1, num2)
+    print("The sum of", num1, "and", num2, "is:", result)
+
+if __name__ == "__main__":
+    main()
 
