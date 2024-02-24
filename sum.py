0e71afe975c019ecd85ccb84d72b528d275b6c7a
akhil2099 <akhilvimal7@gmail.com>
2024-02-24 21:08:16 +0530

    SonarQube testing 4

--- sum.py
+++ sum.py
@@ -1,12 +1,13 @@
-d ef add_numbers(a, b):
-    re turn a + b
-
 def main():
-    num1 = 5
-    num = 3
-    result = add_numbers(num1, num2)
-    print("The sum of", num1, "and", num2, "is:", result)
+    # Prompt user to enter the first number
+    num1 = float(input("Enter the first number: "))
+
+    # Prompt user to enter the second number
+    num2 = float(input("Enter the second number: "))
+
+    # Print the two numbers
+    print("The first number is:", num1)
+    print("The second number is:", num2)
 
 if __name__ == "__main__":
     main()
-
